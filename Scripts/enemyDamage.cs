1a910e503d38077c27204821d7b28d4a612204f4
MoyNunez <manc1221@gmail.com>
2016-05-27 13:58:45 -0500

    prueba sistema de daño al enemigo

---
+++ Scripts/enemyDamage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class enemyDamage : MonoBehaviour{
+
+	int enemyHealth = 50;
+	int TheDamage = 10;
+		
+	public void Ouch (){
+		enemyHealth -= TheDamage;
+		print ("Vida: "+enemyHealth);
+		if(enemyHealth<=0){
+			imDead ();
+		}
+
+		}
+	void imDead(){
+		Destroy (gameObject);
+	}
+}
+
---
+++ Scripts/rayCastForward.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rayCastForward : MonoBehaviour {
+	public enemyDamage _enemyDamage;
+	public float TheDistance;
+	public float maxDistance;
+	// Update is called once per frame
+
+		void Update(){
+		if(Input.GetButtonDown("Fire1")){
+			//print ("Feuer!");
+			RaycastHit hit;
+
+			Vector3 forward = transform.TransformDirection(Vector3.forward) * 20;
+			Debug.DrawRay (transform.position,forward,Color.green);
+
+		if (Physics.Raycast (transform.position, (forward), out hit))
+			TheDistance = hit.distance;
+			print(TheDistance + " " + hit.collider.gameObject.name);
+
+			if(TheDistance<maxDistance){
+				_enemyDamage.Ouch ();
+
+			}
+			}
+		}
+	}
+
